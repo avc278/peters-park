1449d3f2e4f94a60ef3b70dffb008193690bb409
Andres Callanaupa <avc278@nyu.edu>
2020-03-21 17:34:32 -0400

    cats redux tests done

--- src/client/store/cats.js
+++ src/client/store/cats.js
@@ -2,30 +2,27 @@ import axios from 'axios'
 
 
 // ACTION TYPES
-// YOU MAKE THEM!!
+const LOAD_CATS = 'LOAD_CATS';
 
 // INITIAL STATE
-// what would be a good initial state?
-// :thinking:
-const initialState = null
+const initialState = [];
 
 // ACTION CREATORS
 export const loadCats = data => ({
-    // some stuff might go in here? hmmmmm.....
+    type: LOAD_CATS,
+    cats: data
 })
-
 
 // THUNK CREATORS
 export const fetchCats = () => async dispatch => {
-    // YOUR CODE HERE
+    const catsData = (await axios.get('/api/cats/')).data;
+    dispatch(loadCats(catsData));
 }
 
 // REDUCER
-// just modify inside the switch statement by adding cases.
-// don't modify what the function takes
 export default function (state = initialState, action) {
     switch (action.type) {
-
+        case LOAD_CATS: return action.cats
         default: return state;
     }
 }
